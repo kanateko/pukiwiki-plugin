980113608a6431e131beb8de1e5588dd6ba0ffb7
kanateko <43934345+kanateko@users.noreply.github.com>
2021-06-17 14:03:03 +0900

    v0.2
    他ページの添付画像表示機能追加
    一覧画像の横幅指定機能追加

--- gallery.inc.php/gallery.inc.php
+++ gallery.inc.php/gallery.inc.php
@@ -2,12 +2,14 @@
 /**
  * 画像のギャラリー表示プラグイン
  * 
- * @version 0.1
+ * @version 0.2
  * @author kanateko
  * @link https://jpngamerswiki.com/?f51cd63681
  * @license http://www.gnu.org/licenses/gpl.ja.html GPL
  * -- Updates --
- * 2021-06-16 初版作成 (ver 0.1)
+ * 2021-06-17 他のページの添付画像を表示する機能を追加
+ *            画像の横幅を指定する機能を追加
+ * 2021-06-16 初版作成
  */
 
 function plugin_gallery_init()
@@ -23,23 +25,52 @@ function plugin_gallery_convert()
     global $vars;
     static $gallery_counts = 0;
 
+    $_err = array (
+        'unknown'  =>  '#gallery Error: Unknown argument -> ',
+    );
+
+    $option = array (
+        'width'    =>  '300',
+    );
+
     $args = func_get_args();
     $images = array_pop($args);
     $images = str_replace("\r", "\n", str_replace("\r\n", "\n", $images));
     preg_match_all('/.+\.(?:jpe?g|png|gif)>?.*?\n/i', $images, $matches);
 
     $item = '';
-    $url = get_base_uri() . '?plugin=ref&amp;page=' . $vars['page'];
+    $url_base = get_base_uri() . '?plugin=ref&amp;page=';
 
+    // オプション判別
+    if (!empty($args)) {
+        foreach ($args as $arg) {
+            if (preg_match('/(width)=(\d+)/', $arg, $match)) {
+                $option[$match[1]] = $match[2];
+            } else {
+                return $_err['unknown'] . $arg;
+            }
+        }
+    }
+
+    // ギャラリーアイテムの作成
     foreach ($matches[0] as $image) {
+        // 添付ファイルのあるページを確認
+        if(strpos($image, '/') !== false) {
+            list($page, $image) = explode('/', $image);
+        } else {
+            $page = $vars['page'];
+        }
+        $url = $url_base . $page;
+
+        // キャプションの有無を判別
         if (strpos($image, '>') !== false) {
             list($image, $cap) = explode('>', $image);
-            $item .= '<figure class="gallery-item"><a href="' . $url  . '&src=' . $image .'"
+            $item .= '<figure class="gallery-item" style="width:' . $option['width'] . 'px"><a href="' . $url  . '&src=' . $image .'"
              class="gallery-thumb" data-lightbox="gallery-' . $gallery_counts . '"
               data-title="' . $cap . '"><img class="gallery-source" src="' . $url  . '&src=' . $image .'"></a>
               <figcaption class="gallery-caption">' . $cap . '</figcaption></figure>' . "\n";
         } else {
-            $item .= '<figure class="gallery-item"><a href="' . $url  . '&src=' . $image .'"
+            $item .= '<figure class="gallery-item" style="width:' . $option['width'] . 'px"><a href="' . $url  . '&src=' . $image .'"
              class="gallery-thumb" data-lightbox="gallery-' . $gallery_counts . '">
              <img class="gallery-source" src="' . $url  . '&src=' . $image .'"></a></figure>' . "\n";
         }
