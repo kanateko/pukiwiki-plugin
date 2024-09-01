3bad4f06062727a70f0da131447cbc43cdb0e414
kanateko <jpngamerswiki.contact@gmail.com>
2024-09-01 14:23:57 +0900

    コードを整理
    デフォルト値を設定する機能を追加
    値を複数行にわたって書けるように改善
    値が空白のキーは非表示扱いに変更

--- infobox.inc.php
+++ infobox.inc.php
@@ -2,192 +2,323 @@
 /**
  * テンプレートを読み込んでインフォボックスを設置するプラグイン
  *
- * @version 0.4
+ * @version 1.0.0
  * @author kanateko
  * @link https://jpngamerswiki.com/?f51cd63681
  * @license https://www.gnu.org/licenses/gpl-3.0.html GPLv3
  * -- Update --
- * 2021-08-04 テンプレートの読み込みがループする場合はエラーを表示する機能を追加
- *            テンプレートページの凍結が必要かどうかを設定できる機能を追加
- *            指定した文字列を含む行をinclude時に除外する機能を追加
- * 2021-08-03 初版作成
+ * 2024-09-01 v1.0.0 コードを整理
+ *                   デフォルト値を設定する機能を追加
+ *                   値を複数行にわたって書けるように改善
+ *                   値が空白のキーは非表示扱いに変更
+ * 2023-07-25 v0.6.0 =前後の空白に対応
+ * 2022-03-19 v0.5.0 同ページ内に同じテンプレートを使用したインフォボックスを複数設置できるよう改善
+ * 2021-08-04 v0.4.0 指定した文字列を含む行をinclude時に除外する機能を追加
+ *            v0.3.0 テンプレートページの凍結が必要かどうかを設定できる機能を追加
+ *            v0.2.0 テンプレートの読み込みがループする場合はエラーを表示する機能を追加
+ * 2021-08-03 v0.1.0 初版作成
  */
 
-// テンプレートページ
-define('INFOBOX_TEMPLATE_LOCATION', ':config/plugin/infobox');
-// 使用するのにテンプレートページが凍結されてる必要があるかどうか
-define('INFOBOX_NEED_TO_FREEZE', false);
-
-function plugin_infobox_convert()
+/**
+ * 初期化
+ *
+ * @return void
+ */
+function plugin_infobox_init(): void
 {
-    $args = func_get_args();
-    if (func_num_args() > 0 && strpos(end($args), "\r") !== false) {
-        // 引数が1個以上で最後の引数がマルチラインならインフォボックスを作成
-        $infobox = new Infobox($args);
-    } else {
-        return Infobox::$msg['usage'];
-    }
+    $msg['_infobox_messages'] = [
+        'msg_usage'    => '#infobox([template][,nozoom][,class=xxx][,except=xxx]){{<br>&lt;key&gt; = xxx<br>...<br>}}<br>',
+        'err_notpl'    => '#infobox Error: The template does not exist. (%s)',
+        'err_loop'     => '#infobox Error: The template is already included. (%s)',
+        'err_self'     => '#infobox Error: It is not allowed to load current page as template.',
+        'err_freeze'   => '#infobox Error: Safe mode is enabled. Please freeze template pages.',
+        'err_map'      => '#infobox Error: Could not find multiline argument.'
+    ];
 
-    return $infobox->convert_infobox();
+    set_plugin_messages($msg);
+}
+
+/**
+ * Undocumented function
+ *
+ * @param string ...$args
+ * @return string
+ */
+function plugin_infobox_convert(string ...$args): string
+{
+    $infobox = new PluginInfobox($args);
+
+    return $infobox->convert();
 }
 
 /**
  * インフォボックスの作成
  */
-class Infobox {
-    public static $msg = array(
-        'usage'    => '#infobox([template][,nozoom][,class=xxx][,except=xxx]){{<br>&lt;key&gt; = xxx<br>...<br>}}<br>',
-        'notfound' => '#infobox Error: The template you specified does not exist. -> ',
-        'loop'     => '#infobox Error: The template you specified is already included. -> ',
-        'self'     => '#infobox Error: It is not allowed that loading self as a template.',
-        'freeze'   => '#infobox Error: According to the setting, you should freeze templates before use this plugin.',
-    );
-    private $options = array(
-        'class'  => '',
-        'except' => '',
-    );
-    private $lines;
-    private $template;
+class PluginInfobox {
+    private const KEY_FORMAT = '{{{%s}}}';
+    private const KEY_REGEXP = '([^{}]+)';
+    private const DEFAULT_VALUE_SEPARATOR = ':';
+    private const TEMPLATE_PATH = ':config/plugin/infobox/';
+    private const ENABLE_SAFE_MODE = false;
+    private string $template_page;
+    private array $template;
+    private array $map;
+    private array $options = [];
+    private array $err = [];
+    private static ?array $included;
 
     public function __construct($args)
     {
-        $this->lines = array_pop($args);
-        // オプション判別
-        foreach($args as $i => $arg) {
-            $arg = htmlsc($arg);
-            if ($arg == 'nozoom') {
-                $this->options['class'] .= ' ' . $arg;
-                unset($args[$i]);
-            } else if (strpos($arg, 'class=') !== false) {
-                $this->options['class'] .= ' ' . explode('=', $arg, 2)[1];
-                unset($args[$i]);
-            } else if (strpos($arg, 'except=') !== false) {
-                $this->options['except'] = $this->convert_regexp(explode('=', $arg, 2)[1]);
-                unset($args[$i]);
+        self::$included ??= [];
+        $num = count($args);
+
+        // 引数の分解とエラー処理
+        if ($num < 2) {
+            $this->err = ['msg_usage'];
+        } else {
+            $template_page = self::TEMPLATE_PATH . array_shift($args);
+            $multiline = array_pop($args);
+
+            if (! str_contains($multiline ,"\r")) {
+                // マルチライン無し
+                $this->err = ['err_map'];
+            } elseif (! is_page($template_page)) {
+                // テンプレート無し
+                $this->err = ['err_notpl' => $template_page];
+            } else {
+                $this->template_page = $template_page;
+                $this->parse_options($args);
+                $this->parse_template($template_page);
+                $this->parse_keyvals($multiline);
             }
         }
-        // テンプレートページの指定
-        $this->template = new IncludeTemplate(array_shift($args));
     }
 
     /**
-     * テンプレートの置き換え
+     * HTMLへの変換
+     *
+     * @return string
      */
-    public function convert_infobox()
+    public function convert(): string
     {
-        $add_class = $this->options['class'];
-        $keywords = $this->get_keywords($this->lines);
-        $template = $this->template->get_template();
-        if (! is_array($template)) {
-            // 配列以外ならエラーとして出力
-            return $template;
+        if ($this->has_error()) return $this->show_msg();
+
+        $html = '';
+        $source = $this->template;
+        $class = $this->options['class'] ?? '';
+
+        foreach($this->map as $key => $val) {
+            $search = str_replace('%s', $key, self::KEY_FORMAT);
+            $source = str_replace($search, $val, $source);
         }
 
-        // キーを値に置き換え
-        foreach($keywords as $key => $val) {
-            $template = str_replace('{{{' . $key . '}}}', $val, $template);
-        }
-
-        // 不要部分の削除
-        $infobox = $this->trim_disused($template);
-
+        // HTMLを整形
+        $html = $this->strip_lines($source);
+        $html = convert_html($html);
         $html = <<<EOD
-        <div class="infobox$add_class">
-            $infobox
+        <div class="infobox$class">
+        $html
         </div>
         EOD;
+
+        self::$included[$this->template_page] = null;
 
         return $html;
     }
 
     /**
-     * マルチライン部分をキーと値に分離して配列に格納する
+     * 不要な行の削除
+     *
+     * @param array $source
+     * @return string
      */
-    private function get_keywords($lines)
+    public function strip_lines(array $source): string
     {
-        $keywords = array();
-        $lines = explode("\r", $lines);
-
-        foreach ($lines as $str) {
-            list($key, $val) = explode('=', $str, 2);
-            $keywords[$key] = $val;
+        // 未使用のキー削除
+        foreach ($source as $i => $line) {
+            $pattern = '/' . str_replace('%s', self::KEY_REGEXP, self::KEY_FORMAT) . '/';
+            if (preg_match($pattern, $line)) {
+                unset($source[$i]);
+            }
         }
 
-        return $keywords;
+        $html = implode($source);
 
+        // noinlude部分を削除
+        $html = preg_replace('/==noinclude==[\s\S]*?==\/noinclude==\s/', '', $html);
+
+        return $html;
     }
 
     /**
-     * 除外用の正規表現を組み立てる
+     * テンプレートを取得
+     *
+     * @param string $template_page
+     * @return string
      */
-    private function convert_regexp($str)
-    {
-        $str = str_replace('/', '\/', $str);
-        if (preg_match('/^\^/', $str)) {
-            return '/^(' . preg_replace('/^\^/', '', $str) . ')[\s\S]*$/';
-        } else if (preg_match('/[^\\]\$$/', $str)) {
-            return '/^[\s\S]*(' . preg_replace('/\$$/', '', $str) . ')$/';
-        } else {
-            return '/^[\s\S]*(' . $str . ')[\s\S]*$/';
-        }
-    }
-
-    /**
-     * テンプレートの不要な部分を削除
-     */
-    private function trim_disused($infobox)
-    {
-        $infobox = preg_replace("/^[\s\S]*{{{[^}]*}}}[\s\S]*$/", '', $infobox);
-        $infobox = preg_replace('/^#author[\s\S]*$/', '', $infobox);
-        if ($regexp = $this->options['except']) {
-            $infobox = preg_replace($regexp, '', $infobox);
-        }
-        $infobox = implode($infobox);
-        $infobox = preg_replace('/==noinclude==[\s\S]*?==\/noinclude==\s/', '', $infobox);
-        $infobox = convert_html($infobox);
-
-        return $infobox;
-    }
-}
-
-/**
- * テンプレートページの読み込み
- */
-class IncludeTemplate
-{
-    private static $included = array();
-    private $template_page;
-
-    public function __construct($template)
-    {
-        $this->template_page = $template ? INFOBOX_TEMPLATE_LOCATION . '/' . $template : INFOBOX_TEMPLATE_LOCATION;
-    }
-
-    /**
-     * テンプレートページのソースを取得
-     */
-    public function get_template()
+    public function parse_template(string $template_page): void
     {
         global $vars;
-        $page = $this->template_page;
+
+        $template = [];
+        $current_page = $vars['page'];
 
         // エラーチェック
-        if (! is_page($page)) {
-            // テンプレートが存在しなければエラー
-            return Infobox::$msg['notfound'] . htmlsc($page);
-        } else if (INFOBOX_NEED_TO_FREEZE && ! is_freeze($page)) {
-            // テンプレートの凍結が必要な設定で凍結されてなければエラー
-            return Infobox::$msg['freeze'];
-        } else if (isset(self::$included[$page])) {
-            // テンプレートが既に1回読み込まれていればエラー
-            return Infobox::$msg['loop'] . htmlsc($page);
-        } else if ($page == $vars['page']) {
-            // テンプレートページ内で自分自身が呼び出されていればエラー
-            return Infobox::$msg['self'];
-        } else {
-            self::$included[$page] = true;
-            $template = get_source($page);
-            return $template;
+        if (self::$included[$template_page] === true) {
+            // ループ
+            $this->err = ['err_loop' => $template_page];
+        } elseif ($current_page === $template_page) {
+            // ループ (現ページ)
+            $this->err = ['err_self'];
+        } elseif (self::ENABLE_SAFE_MODE && is_freeze($template_page)) {
+            // セーフモード
+            $this->err = ['err_freeze'];
         }
+
+        // 問題なければテンプレートを取得
+        if (! $this->has_error()) {
+            self::$included[$template_page] = true;
+            $template = get_source($template_page);
+
+            // 不要な行を削除
+            foreach ($template as $i => $line) {
+                // 不要なプラグイン
+                if (str_starts_with($line, '#author') || str_starts_with($line, '#freeze')) {
+                    unset($template[$i]);
+                }
+
+                // 除外設定
+                if ($this->options['except'] !== null) {
+                    if (preg_match($this->options['except'], $line)) {
+                        unset($template[$i]);
+                    }
+                }
+            }
+
+            // デフォルト値の取得と置き換え
+            $template = $this->get_default_values($template);
+        }
+
+        $this->template = $template;
+    }
+
+    /**
+     * デフォルト値の取得と置き換え
+     *
+     * @param array $template
+     * @return array
+     */
+    public function get_default_values(array $template): array
+    {
+        $key_regexp = self::KEY_REGEXP;
+        $separator = self::DEFAULT_VALUE_SEPARATOR;
+        $regexp = '/' . str_replace('%s', $key_regexp .  $separator . $key_regexp, self::KEY_FORMAT) . '/';
+
+        // デフォルト値の取得
+        foreach ($template as $i => $line) {
+            if (preg_match($regexp, $line, $m)) {
+                $this->map[$m[1]]= $m[2];
+                $replace = str_replace('%s', $m[1], self::KEY_FORMAT);
+                $template[$i] = str_replace($m[0], $replace, $line);
+            }
+        }
+
+        return $template;
+    }
+
+    /**
+     * マルチライン引数からキーと値を取得する
+     *
+     * @param string $multiline
+     * @return void
+     */
+    public function parse_keyvals(string $multiline): void
+    {
+        $lines = explode("\r", trim($multiline));
+        $current_key = '';
+
+        foreach ($lines as $line) {
+            if (preg_match('/(.+?)\s*(?<!\\\)=\s*(.+)?/', $line, $m)) {
+                $current_key = '';
+
+                if ($m[2] !== null) {
+                    $this->map[$m[1]] = $m[2];
+                } else {
+                    // 複数行のキーを設定
+                    $current_key = $m[1];
+                }
+            } elseif ($current_key !== '') {
+                // 複数行での値指定
+                $this->map[$current_key] = $this->map[$current_key] === null ? '' : $this->map[$current_key] . "&br;";
+                $this->map[$current_key] .= str_replace('\=', '=', $line);
+            }
+        }
+
+    }
+
+    /**
+     * オプションの判別
+     *
+     * @param array $args
+     * @return void
+     */
+    public function parse_options(array $args): void
+    {
+        $class_array = [];
+
+        foreach($args as $arg) {
+            [$key, $val] = array_map('trim', explode('=', $arg));
+
+            if ($val !== null) {
+                if ($key === 'except') {
+                    // 正規表現での除外設定
+                    $this->options['except'] = '/' . str_replace('/', '\/', $val) . '/';
+                } elseif ($key === 'class') {
+                    // 追加クラス
+                    $class_array[] = $val;
+                }
+            } elseif ($key === 'nozoom') {
+                // 追加クラス
+                $class_array[] = $val;
+            }
+        }
+
+        // クラスの整形
+        if ($class_array !== []) {
+            $this->options['class'] = ' ' . htmlsc(implode(' ', $class_array));
+        }
+    }
+
+    /**
+     * エラーの確認
+     *
+     * @return boolean
+     */
+    public function has_error(): bool
+    {
+        return $this->err !== [];
+    }
+
+    /**
+     * メッセージ表示
+     *
+     * @return string
+     */
+    public function show_msg(): string
+    {
+        global $_infobox_messages;
+
+        $msg = '';
+
+        if (array_values($this->err) === $this->err) {
+            $msg = $_infobox_messages[$this->err[0]];
+        } else {
+            foreach ($this->err as $key => $val) {
+                $msg = $_infobox_messages[$key];
+                $msg = str_replace('%s', htmlsc($val), $msg);
+            }
+        }
+
+        return "<p>$msg</p>";
     }
 }
