64f088185e104e50684f58f55069a30f16e0c874
kanateko <jpngamerswiki.contact@gmail.com>
2025-08-07 17:00:18 +0900

    PukiWikiの日付フォーマットを追加

--- datetime.inc.php
+++ datetime.inc.php
@@ -2,11 +2,12 @@
 /**
  * 指定したフォーマットで日付や時間を表示するプラグイン
  *
- * @version 1.0.0
+ * @version 1.0.1
  * @author kanateko
  * @link https://jpngamerswiki.com/?f51cd63681
  * @license https://www.gnu.org/licenses/gpl-3.0.html GPLv3
  * -- Updates --
+ * 2025-08-07 v1.0.1 PukiWikiの日付フォーマットを追加 (DATE_PKWK)
  * 2025-08-06 v1.0.0 初版作成
  */
 
@@ -22,23 +23,33 @@ function plugin_datetime_inline(string ...$args): string
 
     if (empty($args)) return format_date(UTIME);
 
-    $format = match($args[0]) {
-        'DATE_ATOM'    => DATE_ATOM,
-        'DATE_COOKIE'  => DATE_COOKIE,
-        'DATE_ISO8601' => DATE_ISO8601,
-        'DATE_RFC822'  => DATE_RFC822,
-        'DATE_RFC850'  => DATE_RFC850,
-        'DATE_RFC1036' => DATE_RFC1036,
-        'DATE_RFC1123' => DATE_RFC1123,
-        'DATE_RFC7231' => DATE_RFC7231,
-        'DATE_RFC2822' => DATE_RFC2822,
-        'DATE_RFC3339' => DATE_RFC3339,
-        'DATE_RSS'     => DATE_RSS,
-        'DATE_W3C'     => DATE_W3C,
-        default        => htmlsc($args[0])
-    };
+    $has_timestamp = count($args) > 1 && is_numeric($args[1]);
+    $datetime = '';
 
-    $time = $args[1] ? (int)$args[1] : UTIME;
+    if ($args[0] === 'DATE_PKWK') {
+        // PukiWikiの日付フォーマット
+        $timestamp = $has_timestamp ? (int)$args[1] - LOCALZONE : UTIME;
+        $datetime = format_date($timestamp);
+    } else {
+        // 定義済みor手動指定フォーマット
+        $timestamp = $has_timestamp ? (int)$args[1] : UTIME;
+        $format = match($args[0]) {
+            'DATE_ATOM'    => DATE_ATOM,
+            'DATE_COOKIE'  => DATE_COOKIE,
+            'DATE_ISO8601' => DATE_ISO8601,
+            'DATE_RFC822'  => DATE_RFC822,
+            'DATE_RFC850'  => DATE_RFC850,
+            'DATE_RFC1036' => DATE_RFC1036,
+            'DATE_RFC1123' => DATE_RFC1123,
+            'DATE_RFC7231' => DATE_RFC7231,
+            'DATE_RFC2822' => DATE_RFC2822,
+            'DATE_RFC3339' => DATE_RFC3339,
+            'DATE_RSS'     => DATE_RSS,
+            'DATE_W3C'     => DATE_W3C,
+            default        => htmlsc($args[0])
+        };
+        $datetime = date($format, $timestamp);
+    }
 
-    return date($format, $time);
+    return $datetime;
 }
